aced8d3abc4048ff28618ce6535ca143fb127ff1
Rushikesh Shirurkar <24334001@sau.edu.in>
2026-09-24 15:49:34 +0530

    commit

--- src/Login.js
+++ src/Login.js
@@ -19,7 +19,7 @@ function Login() {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://spicer-backend.onrender.com/api/auth/login",
         formData
       );
 
--- src/Register.js
+++ src/Register.js
@@ -26,7 +26,7 @@ function Register() {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        "https://spicer-backend.onrender.com/api/auth/register",
         {
           username: formData.username,
           email: formData.email,
